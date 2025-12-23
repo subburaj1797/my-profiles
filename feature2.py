aa06ad900f2678a8582d00c58cda1c020a726dca
dev1 <dev1@gmail.com>
2025-12-23 06:36:46 +0000

    revert lab

--- feature2.py
+++ feature2.py
@@ -1,1 +1,2 @@
-print("hello it's feature2 developed by dev2 - i. edit from dev1 edited dev2")
+print("hello it's feature2 developed by dev2 - i. edit from dev1 edited dev2"i)
+print ("revert feature1")
